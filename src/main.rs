9447e0be9b74653ba83b2e37da3b37727ad6be94
Joseph Sungpil Choi <chousemath@gmail.com>
2017-05-09 18:39:49 +0900

    added some more comments to the tutorial guessing game 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
+// bring input/output library into scope (from standard library)
+// bring libraries into scope explicity with `use` keyword
+use std::io;
+
+// main function is entry point into program
 fn main() {
-    let v = vec![1,2,3];
-    println!("The length of v is {}", v.len());
-    let v2 = v;
-    println!("The length of v2 is now {}", v2.len());
+    // println! is a macro
+    println!("Guess the number I am thinking of");
+    println!("Please enter a number: ");
+    // the `let` keyword is used to create variables
+    // in rust, variables are immutable by default, but the `mut` keyword allows mutability
+    // :: -> association, `new` is associated function of `String` type (static method)
+    let mut guess = String::new();
+    // call the `stdin` method from the `io` library on first line of program
+    // io::stdin() returns handle to standard input for terminal
+    // .readline(str x) gets input from user and returns io::Result
+    // string target of .readline(str x) must be mutable to accept value
+    // & -> reference, and references are immutable by default, therefore need &mut
+    io::stdin().read_line(&mut guess)
+               .expect("Failed to read in line");
+    // {} is a placeholder that holds a value in place (string interpolation)
+    println!("You guess {}", guess);
+    string_interpolation();
+}
+
+fn string_interpolation() {
+    let x = "Joseph";
+    let y = "Choi";
+    // multiple string interpolation values can be used so long as they are in order
+    println!("My full name is {} {}", x, y);
 }
