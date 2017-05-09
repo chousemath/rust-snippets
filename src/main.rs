b59a545f09420365077355a91b370af37bda7ea1
Joseph Sungpil Choi <chousemath@gmail.com>
2017-05-09 19:10:06 +0900

    added pattern matching to the guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -9,6 +10,7 @@ fn main() {
     let lower_range = 1;
     let upper_range = 100;
     let secret_number = rand::thread_rng().gen_range(lower_range, upper_range + 1);
+    let secret_string = secret_number.to_string();
     println!("Please guess a number between {} and {}: ", lower_range, upper_range);
     // in rust, variables are immutable by default, but the `mut` keyword allows mutability
     // :: -> association, `new` is associated function of `String` type (static method)
@@ -20,8 +22,14 @@ fn main() {
     // & -> reference, and references are immutable by default, therefore need &mut
     io::stdin().read_line(&mut guess)
                .expect("Failed to read in line");
-    // {} is a placeholder that holds a value in place (string interpolation)
     println!("You guessed {}", guess);
+
+    match guess.cmp(&secret_string) {
+        Ordering::Less    => println!("Your guess was too small..."),
+        Ordering::Greater => println!("Your guess was too large..."),
+        Ordering::Equal   => println!("Your guess was correct, you win!")
+    }
+
     println!("The secret number was {}", secret_number);
     string_interpolation();
 }
@@ -29,6 +37,7 @@ fn main() {
 fn string_interpolation() {
     let x = "Joseph";
     let y = "Choi";
+    // {} is a placeholder that holds a value in place (string interpolation)
     // multiple string interpolation values can be used so long as they are in order
     println!("My full name is {} {}", x, y);
 }
